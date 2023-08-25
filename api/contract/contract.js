5e02b21d6dedf55a8e0e274e8c33a77ce16f4391
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2023-08-25 17:22:58 +0530

    Update contract.js

--- api/contract/contract.js
+++ api/contract/contract.js
@@ -1,7 +1,7 @@
 const {Web3}= require("web3");
 const ABI = require("../ABI.json");
 const web3 = new Web3("ADD YOUR API KEY")
-const contractAddress = "0x795e47574f11055806e294f1b95090cd46ececdd";
+const contractAddress = "ADD YOUR TASK CONTRACT ADDRESS";
 const contract = new web3.eth.Contract(ABI,contractAddress);
 
 module.exports={contract}
